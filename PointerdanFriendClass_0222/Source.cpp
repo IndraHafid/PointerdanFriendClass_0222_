9db039569a2ec2423262c47219f60ce5a2d3a3f3
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-07 15:58:53 +0700

    Menerapkan dereferensi pointer di kelas siswa

--- PointerdanFriendClass_0222/Source.cpp
+++ PointerdanFriendClass_0222/Source.cpp
@@ -9,3 +9,17 @@ public:
         cout << "nomor induk :" << nim << endl;
     }
 };
+
+int main() {
+    mahasiswa mhs(1); //Object mhs
+    mhs.shownim(); //Member Accsess Operator
+
+    mahasiswa &refmhs = mhs; //Pointer Reference refMhs
+    refmhs.nim = 2; //Member Access Operator
+    mhs.shownim();
+
+    mahasiswa* pmhs = &mhs; //Pointer Dereference pMhs
+    pmhs->nim = 3; //Arrow Pointer
+    pmhs->shownim();
+    return 0;
+}
