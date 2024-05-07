c2a26599465e03e9ba885b7e4c05becbc8e7479e
IndraHfid <indrahafidsaputra@gmail.com>
2024-05-07 16:06:10 +0700

    menginput nim mhs

--- PointerdanFriendClass_0222/Source1.cpp
+++ PointerdanFriendClass_0222/Source1.cpp
@@ -9,3 +9,12 @@ public:
         cout << "no induk = " << nim << endl;
     }
 };
+
+int main()
+{
+    mahasiswa* mhs = new mahasiswa{ 1 };
+    mhs->nim = 2;
+    mhs->shownim();
+    delete mhs;
+    return 0;
+}
